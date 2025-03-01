f67494ce31a0cbdfa31f7e214431e350b4e08b01
Neilander <yw6472@nyu.edu>
2025-03-01 17:58:57 -0500

    spike finish

---
+++ Assets/Resources/Neil/Neil04Spike.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Neil04Spike : Tile
+{
+    public TileTags OtherTargetWeDamage;
+    public float damageForce;
+    private Tile follow;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Tile>() != null)
+        {
+            Tile tar = collision.gameObject.GetComponent<Tile>();
+            if (tar != follow && tar.hasTag(OtherTargetWeDamage))
+            {
+                tar.takeDamage(this, 1);
+                Vector2 toOtherTile = (Vector2)tar.transform.position - (Vector2)transform.position;
+                toOtherTile.Normalize();
+                tar.addForce(damageForce * toOtherTile);
+                follow = tar;
+                transform.parent = follow.transform;
+                Destroy(GetComponent<Rigidbody2D>());
+            }
+        }
+    }
+}
